9a6fc742765b54f017e06d5927942f439a77218b
dpcc2017 <32982147+dpcc2017@users.noreply.github.com>
2018-09-23 16:11:04 +0800

    Add files via upload

---
+++ user_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user_table` (`id`, `username`, `password`, `age`, `sex`) VALUES (1, '李明', '123456', 20, NULL);
+INSERT INTO `user_table` (`id`, `username`, `password`, `age`, `sex`) VALUES (2, '??', '000000', 0, '?');
+INSERT INTO `user_table` (`id`, `username`, `password`, `age`, `sex`) VALUES (3, 'liming', '12345', NULL, NULL);
+INSERT INTO `user_table` (`id`, `username`, `password`, `age`, `sex`) VALUES (6, '??', NULL, 25, NULL);
